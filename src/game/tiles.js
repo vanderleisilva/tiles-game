ce4d7fcbb574b3da6871638dd23ceb0d672cf159
Vanderlei Alves da Silva <vanderlei.alves.da.silva@gmail.com>
2019-10-11 14:44:15 +0200

    removal of unused function

--- src/game/tiles.js
+++ src/game/tiles.js
@@ -1,21 +1,10 @@
 var colors = ["B", "R", "Y"];
 
-const colorScore = square =>
-  colors
-    .map(color => ({
-      size: square
-        .map(l => l.filter(c => c.color === color && !c.origin).length)
-        .reduce((total, num) => total + num, 0),
-      color
-    }))
-    .sort((a, b) => a.size - b.size);
-
 const createMap = (columns, lines) =>
   Array.from({ length: columns }).map((v, l) =>
     Array.from({ length: lines }).map((x, c) => ({
       color: colors[Math.floor(Math.random() * colors.length)],
-      origin: l + c == 0,
-      id: `${l}${c}`
+      origin: l + c == 0
     }))
   );
 
