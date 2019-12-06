0a834e688f1fb4f141f1430b7633482cccbc60a3
Vanderlei Alves da Silva <vanderlei.alves.da.silva@gmail.com>
2019-12-06 17:10:07 +0100

    Updates

---
+++ precache-manifest.fde9e3d2fcc01597b249242e784224a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74cad5f40db51085879e2766b01f6fa",
+    "url": "/tiles-game/index.html"
+  },
+  {
+    "revision": "2aae384a72744565dc80",
+    "url": "/tiles-game/static/css/main.e5baa048.chunk.css"
+  },
+  {
+    "revision": "f1338011711658117a00",
+    "url": "/tiles-game/static/js/2.8e79adb5.chunk.js"
+  },
+  {
+    "revision": "2aae384a72744565dc80",
+    "url": "/tiles-game/static/js/main.daee653c.chunk.js"
+  },
+  {
+    "revision": "2009e5eda975e0c20f71",
+    "url": "/tiles-game/static/js/runtime-main.3d891cef.js"
+  }
+]);
